1bd66221ebaec3eb4b2c1d570b390f8145ed18d5
gogogous88@gmail.com <gogogous88@gmail.com>
2017-12-26 00:38:30 -0500

    hello

--- backend/passport.js
+++ backend/passport.js
@@ -68,7 +68,8 @@ const passportConfig = app => {
       {
         clientID: keys.GOOGLE_CLIENT_ID,
         clientSecret: keys.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
+        callbackURL: "/auth/google/callback",
+        proxy: true
       },
       async (accessToken, refreshToken, profile, done) => {
         const existingUser = await User.findOne({ googleId: profile.id });
