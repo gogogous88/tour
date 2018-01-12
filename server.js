6b990517a65798c0aab68ed8676352565261d83d
gogogous88@gmail.com <gogogous88@gmail.com>
2018-01-11 19:23:35 -0500

    http try to point to https only

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ const app = express();
 // apply express configs
 require("./backend/express")(app, serverConfigs);
 
+//mark added to force all http point to https on process.env enviroment:
+app.use(function(req, res, next) {
+  var sslUrl;
+
+  if (
+    process.env.NODE_ENV === "production" &&
+    req.headers["x-forwarded-proto"] !== "https"
+  ) {
+    sslUrl = ["https://www.tourgai.com", req.url].join("");
+    return res.redirect(sslUrl);
+  }
+
+  return next();
+});
+
 // fire up the server
 app.listen(serverConfigs.PORT, "0.0.0.0", error => {
   if (error) throw error;
