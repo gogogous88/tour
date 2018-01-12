782bbe331f92b2507b6e9c34fe339620c1561554
gogogous88@gmail.com <gogogous88@gmail.com>
2018-01-11 19:42:24 -0500

    server.js added process.env https only

--- server.js
+++ server.js
@@ -13,23 +13,18 @@ mongoose.connect(serverConfigs.DBURL);
 // initialize express
 const app = express();
 
+//mark added to force all http point to https on process.env enviroment:
+if (process.env.NODE_ENV === "production") {
+  // only redirect in prod
+  app.use(function(req, res, next) {
+    if (req.headers["x-forwarded-proto"] != "https")
+      res.redirect(["https://", req.get("Host"), req.url].join(""));
+    else next();
+  });
+}
+
 // apply express configs
 require("./backend/express")(app, serverConfigs);
-
-//mark added to force all http point to https on process.env enviroment:
-app.use(function(req, res, next) {
-  var sslUrl;
-
-  if (
-    process.env.NODE_ENV === "production" &&
-    req.headers["x-forwarded-proto"] !== "https"
-  ) {
-    sslUrl = ["https://www.tourgai.com", req.url].join("");
-    return res.redirect(sslUrl);
-  }
-
-  return next();
-});
 
 // fire up the server
 app.listen(serverConfigs.PORT, "0.0.0.0", error => {
